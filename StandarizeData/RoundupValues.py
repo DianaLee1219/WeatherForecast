9324332f0c09a229fcc8e541473abf0ee9efcc24
DianaLee1219 <ldh05930@gmail.com>
2025-03-17 00:37:57 -0600

    Update RoundupValues.py

--- StandarizeData/RoundupValues.py
+++ StandarizeData/RoundupValues.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-# Load the data from the Excel file
-file_path = 'cleaned_data.cvs'
-
-try:
-    df = pd.read_csv(file_path, encoding="utf-8")
-except UnicodeDecodeError:
-    df = pd.read_csv(file_path, encoding="ISO-8859-1") 
-
+# Load the data from the csv file
+df = pd.read_csv("cleaned_data.csv", encoding="ISO-8859-1")
 
 # Function to round latitude and longitude to 0.1 resolution
 def round_coordinates(value):
@@ -17,12 +11,8 @@ def round_coordinates(value):
 df['latitude'] = df['latitude'].apply(round_coordinates)
 df['longitude'] = df['longitude'].apply(round_coordinates)
 
-# Print the first 10 rows of the modified table
-print("First 10 rows with rounded latitude and longitude:")
-print(df.head(10).to_string(index=False))
-
 # Save the modified DataFrame to a new Excel file
-output_file_path = 'WeatherForecast_rounded.cvs'
-df.to_excel(output_file_path, index=False, sheet_name=sheet_name)
+output_file_path = 'WeatherForecast_rounded.csv'
+df.to_csv(output_file_path, index=False, encoding="ISO-8859-1")
 
 print(f"\nModified data saved to {output_file_path}")
