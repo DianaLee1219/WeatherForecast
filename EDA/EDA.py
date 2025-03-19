3cf19340823097406a35d3f6f0c91d90914ec131
DianaLee1219 <ldh05930@gmail.com>
2025-03-19 00:55:21 -0600

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -5,15 +5,8 @@ import random
 from sklearn.preprocessing import MinMaxScaler
 
 # Load the data
-file_path = "WeatherForecast_CleanedOutliers.csv"
+file_path = "EDA_cleaned_weather_data.csv"
 df = pd.read_csv(file_path, encoding="ISO-8859-1")
-
-scaler = MinMaxScaler()
-numerical_columns = ["degC", "wind_kph", "pressure_mb", "precip_mm", "humidity", "feels_like_celsius", "visibility_km", 
-                     "uv_index", "air_quality_Ozone", "air_quality_Nitrogen_dioxide", "air_quality_PM2.5"]
-df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
-
-df.to_csv("EDA_cleaned_weather_data.csv", index=False)
 
 # Select only numeric columns
 numeric_data = df.select_dtypes(include=['number'])
